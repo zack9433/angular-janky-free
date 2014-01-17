1a55d763b164a84c1280d8e1a3c79edbd6630fa7
zackyang <zack9433@gmail.com>
2014-01-17 09:43:41 +0800

    First commit

---
+++ angular-janky-free.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * Created by ZackCF_Yang on 2014/1/17.
+ */
+
+'use strict';
+
+angular.module('ngJankyFree')
+    .directive('ngJankyFree', function ($timeout, $log) {
+        return {
+            restrict: 'A',
+            link: function postLink(scope, element) {
+                var timer,
+                    jqWin = angular.element(window);
+
+                if ('body' === element[0].nodeName.toLowerCase()) {
+                    jqWin.on('scroll', function() {
+                        $timeout.cancel(timer);
+                        if (!element.hasClass('disable-hover')) {
+                            element.addClass('disable-hover');
+                        }
+
+                        timer = $timeout(function() {
+                            element.removeClass('disable-hover');
+                        }, 500);
+                    });
+                } else {
+                    $log.error('The element must be body!');
+                }
+            }
+        };
+    });
